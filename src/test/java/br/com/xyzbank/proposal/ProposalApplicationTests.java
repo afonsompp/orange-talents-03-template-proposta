b8d650cd66598f6faee23419cef884cbe42d3f30
Afonso Mateus <afonsoprad10@gmail.com>
2021-04-12 09:25:53 -0400

    setup spring

---
+++ src/test/java/br/com/xyzbank/proposal/ProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xyzbank.proposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
