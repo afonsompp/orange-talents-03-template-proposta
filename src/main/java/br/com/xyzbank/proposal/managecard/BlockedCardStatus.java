bb3e97f270acf7911cecfa2299e21a42f461c072
Afonso Mateus <afonsoprad10@gmail.com>
2021-04-20 15:06:44 -0400

    add json creator in enum method

--- src/main/java/br/com/xyzbank/proposal/managecard/BlockedCardStatus.java
+++ src/main/java/br/com/xyzbank/proposal/managecard/BlockedCardStatus.java
@@ -1,11 +1,14 @@
 package br.com.xyzbank.proposal.managecard;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum BlockedCardStatus {
 
 	WAITING,
 	BLOCKED;
 
 	@Deprecated
+	@JsonCreator
 	public static BlockedCardStatus fromString(String value) {
 		if (value == null)
 			throw new IllegalArgumentException();
