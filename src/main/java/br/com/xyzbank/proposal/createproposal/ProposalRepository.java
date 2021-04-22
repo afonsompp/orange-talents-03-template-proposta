3f1c2e4823be30bd2edb8e3621fe72c500c38710
Afonso Mateus <afonsoprad10@gmail.com>
2021-04-22 11:55:54 -0400

    remove unsued method query

--- src/main/java/br/com/xyzbank/proposal/createproposal/ProposalRepository.java
+++ src/main/java/br/com/xyzbank/proposal/createproposal/ProposalRepository.java
@@ -1,14 +1,12 @@
 package br.com.xyzbank.proposal.createproposal;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.xyzbank.proposal.managecard.Card;
 
 @Repository
 public interface ProposalRepository extends JpaRepository<Proposal, Long> {
-	Optional<Proposal> findByIdCard(String idCard);
 
 	List<Proposal> findByStatusAndCard(ProposalStatus status, Card card);
 }
